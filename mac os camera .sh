edca974315a4fbe3598d8cb726d6629fec8dae80
ranjanjyoti152 <ranjanjyoti152@gmail.com>
2023-06-17 13:18:14 +0530

    Add files via upload

---
+++ mac os camera .sh
@@ -0,0 +1,15 @@
+# Open Terminal and run the following commands
+cd ~/Downloads/
+chmod +x tccplus
+./tccplus add Microphone com.hnc.Discord
+
+
+
+
+# get app id (Zoom.us used in example):
+$ osascript -e 'id of app "zoom.us"'
+# output: us.zoom.xos
+
+$ sudo sqlite3 ~/Library/Application\ Support/com.apple.TCC/TCC.db "INSERT or REPLACE INTO access VALUES('kTCCServiceMicrophone','us.zoom.xos',0,2,0,1,NULL,NULL,NULL,'UNUSED',NULL,0,1541440109);"
+
+$ sudo sqlite3 ~/Library/Application\ Support/com.apple.TCC/TCC.db "INSERT or REPLACE INTO access VALUES('kTCCServiceCamera','us.zoom.xos',0,2,0,1,NULL,NULL,NULL,'UNUSED',NULL,0,1541440109);"
